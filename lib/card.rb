895eb5e1797c77e687d01a7f3336dd55127bbc49
Jo Nuckley <jnuckley@yahoo.co.uk>
2019-11-05 15:09:43 +0000

    NameError for balance - method not created yet

---
+++ lib/card.rb
@@ -0,0 +1,10 @@
+class Oystercard
+def initialize(balance = 0)
+  @balance = balance
+end
+end
+#check balance/starting balance
+
+#add money to Oystercard
+
+#remove money from Oystercard
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,4 +1,8 @@
 require './lib/card'
 
 describe Oystercard do
+  it 'can find a balance' do
+    card = Oystercard.new
+    expect(card).to respond_to(balance)
+  end
 end
