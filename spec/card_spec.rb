4c95288c9121d01604785db8d64b7d6f6db7535c
Jo Nuckley <jnuckley@yahoo.co.uk>
2019-11-05 20:50:45 +0000

    used subject in the test and removed all the additional lines

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,33 +1,27 @@
 require './lib/card'
 
 describe OysterCard do
+  subject(:card) { OysterCard.new }
   it 'can make an instance of the card' do
-    card = OysterCard.new
     expect(card).to be_instance_of(OysterCard)
   end
   it 'can respond to a balance' do
-    card = OysterCard.new
     expect(card).to respond_to(:balance)
   end
   it 'card balance should start at zero' do
-    card = OysterCard.new
     expect(card.balance).to eq(0)
   end
   it 'should respond to deposit' do
-    card = OysterCard.new
     expect(card).to respond_to(:deposit).with(1).argument
   end
   it 'should update the balance with deposits' do
-    card = OysterCard.new
     card.deposit(15)
     expect(card.balance).to eq(15)
   end
   it 'should respond to withdraw' do
-    card = OysterCard.new
     expect(card).to respond_to(:withdraw).with(1).argument
   end
   it 'should update when money is withdrawn' do
-    card = OysterCard.new
     card.deposit(20)
     card.withdraw(15)
     expect(card.balance).to eq(5)
