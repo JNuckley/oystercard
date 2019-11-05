58383e8a338d715fe4577175cac3446b9b4bc34e
Jo Nuckley <jnuckley@yahoo.co.uk>
2019-11-05 20:42:43 +0000

    passing and removed balance method and replaced with attr_reader

--- lib/card.rb
+++ lib/card.rb
@@ -1,20 +1,18 @@
-class Oystercard
+class OysterCard
   attr_reader :balance
-#instance variable to allow balance to be used within the class
-def initialize
-  @balance = 0
-end
-#  balance method to allow updates
-def balance
-  @balance
-end
-#  deposit with single argument
-def deposit(gbp)
-  @balance = @balance + gbp
-end
 
-def withdraw(gbp)
-  # @balance = @balance - gbp
-end
-#remove money from Oystercard
+  # instance variable to allow balance to be used within the class
+  def initialize
+    @balance = 0
+  end
+
+  # deposit with single argument
+  def deposit(gbp)
+    @balance += gbp
+  end
+
+  # remove money from Oystercard
+  def withdraw(gbp)
+    @balance -= gbp
+  end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,35 +1,35 @@
 require './lib/card'
 
-describe Oystercard do
+describe OysterCard do
   it 'can make an instance of the card' do
-    card = Oystercard.new
-    expect(card).to be_instance_of(Oystercard)
+    card = OysterCard.new
+    expect(card).to be_instance_of(OysterCard)
   end
   it 'can respond to a balance' do
-    card = Oystercard.new
+    card = OysterCard.new
     expect(card).to respond_to(:balance)
   end
   it 'card balance should start at zero' do
-    card = Oystercard.new
+    card = OysterCard.new
     expect(card.balance).to eq(0)
   end
   it 'should respond to deposit' do
-    card = Oystercard.new
+    card = OysterCard.new
     expect(card).to respond_to(:deposit).with(1).argument
   end
   it 'should update the balance with deposits' do
-    card = Oystercard.new
+    card = OysterCard.new
     card.deposit(15)
     expect(card.balance).to eq(15)
   end
   it 'should respond to withdraw' do
-    card = Oystercard.new
+    card = OysterCard.new
     expect(card).to respond_to(:withdraw).with(1).argument
   end
   it 'should update when money is withdrawn' do
-    card = Oystercard.new
+    card = OysterCard.new
     card.deposit(20)
     card.withdraw(15)
     expect(card.balance).to eq(5)
   end
-  end
+end
