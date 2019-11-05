e350ad27d68010d237cc46ede77a9493066a5a5c
Jo Nuckley <jnuckley@yahoo.co.uk>
2019-11-05 21:01:26 +0000

    refactored to use one line syntax for respond to and instances

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,25 +2,24 @@ require './lib/card'
 
 describe OysterCard do
   subject(:card) { OysterCard.new }
-  it 'can make an instance of the card' do
-    expect(card).to be_instance_of(OysterCard)
-  end
-  it 'can respond to a balance' do
-    expect(card).to respond_to(:balance)
-  end
+  
+  it { is_expected.to be_instance_of(OysterCard) }
+
+  it { is_expected.to respond_to(:balance) }
+
   it 'card balance should start at zero' do
     expect(card.balance).to eq(0)
   end
-  it 'should respond to deposit' do
-    expect(card).to respond_to(:deposit).with(1).argument
-  end
+
+  it { is_expected.to respond_to(:deposit).with(1).argument }
+
   it 'should update the balance with deposits' do
     card.deposit(15)
     expect(card.balance).to eq(15)
   end
-  it 'should respond to withdraw' do
-    expect(card).to respond_to(:withdraw).with(1).argument
-  end
+
+  it { is_expected.to respond_to(:withdraw).with(1).argument }
+
   it 'should update when money is withdrawn' do
     card.deposit(20)
     card.withdraw(15)
