9d230224600877f468de9dbe8587d81b4d724a81
Jo Nuckley <jnuckley@yahoo.co.uk>
2019-11-06 17:09:29 +0000

    Fail - cannot find the touch_in method

--- lib/journey.rb
+++ lib/journey.rb
@@ -5,4 +5,8 @@ attr_accessor :in_journey
 def initialize
   @in_journey
 end
+
+# def touch_in
+#   @in_journey = true
+# end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -6,4 +6,6 @@ describe Journey do
   it { is_expected.to be_instance_of(Journey) }
 
   it { is_expected.to respond_to(:in_journey) }
+
+  it { is_expected.to respond_to(:touch_in) }
 end
