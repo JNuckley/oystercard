8c397271096ab2aecdaee15f0a45a78a488d32d7
Jo Nuckley <jnuckley@yahoo.co.uk>
2019-11-06 15:11:09 +0000

    Passing test after maximum top-up limit test set

--- lib/card.rb
+++ lib/card.rb
@@ -1,6 +1,6 @@
 class OysterCard
   attr_reader :balance
-
+MAXIMUM_BALANCE = 90
   # instance variable to allow balance to be used within the class
   def initialize
     @balance = 0
@@ -8,11 +8,11 @@ class OysterCard
 
   # deposit with single argument
   def top_up(gbp)
-    @balance += gbp
+  fail 'Cannot exceed #{MAXIMUM_BALANCE}' if @balance + gbp > MAXIMUM_BALANCE
+  @balance += gbp
   end
-
   # remove money from Oystercard
-  def withdraw(gbp)
+  def deduct(gbp)
     @balance -= gbp
   end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -14,15 +14,18 @@ describe OysterCard do
   it { is_expected.to respond_to(:top_up).with(1).argument }
 
   it 'should update the balance with deposits' do
-    card.top_up(15)
-    expect(card.balance).to eq(15)
+    expect{card.top_up 1}.to change{card.balance}.by 1
   end
 
-  it { is_expected.to respond_to(:withdraw).with(1).argument }
+  it 'will not exceed maximum top-up amount 90' do
+    expect{card.top_up 91}.to raise_error('Cannot exceed #{MAXIMUM_BALANCE}')
+  end
+
+  it { is_expected.to respond_to(:deduct).with(1).argument }
 
   it 'should update when money is withdrawn' do
     card.top_up(20)
-    card.withdraw(15)
+    card.deduct(15)
     expect(card.balance).to eq(5)
   end
 end
