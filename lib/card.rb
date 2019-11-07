0ada80fe3ea4070d4cd4abc66835a88fd49c5675
Jo Nuckley <jnuckley@yahoo.co.uk>
2019-11-07 16:22:50 +0000

    combined journey and card rb files because they can't speak to each other :(

--- lib/card.rb
+++ lib/card.rb
@@ -1,11 +1,16 @@
+
+
 class OysterCard
 
 attr_reader :balance
+attr_accessor :in_journey
 
+MINIMUM_BALANCE = 1
 MAXIMUM_BALANCE = 90
   # instance variable to allow balance to be used within the class
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   # deposit with single argument
@@ -16,5 +21,14 @@ MAXIMUM_BALANCE = 90
   # remove money from Oystercard
   def deduct(gbp)
     @balance -= gbp
+  end
+
+  def touch_in
+    fail "Less than £1" if @balance < MINIMUM_BALANCE
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -7,6 +7,8 @@ describe OysterCard do
 
   it { is_expected.to respond_to(:balance) }
 
+  it { is_expected.to respond_to(:in_journey) }
+
   it 'card balance should start at zero' do
     expect(card.balance).to eq(0)
   end
@@ -28,6 +30,33 @@ describe OysterCard do
     card.deduct(15)
     expect(card.balance).to eq(5)
   end
+  it { is_expected.to respond_to(:touch_in) }
 
-  
-end
+  it { is_expected.to respond_to(:touch_out) }
+
+  # it 'touch_in should return true' do
+  #   expect(journey.touch_in).to be(true)
+  # end
+
+  it 'touch out should return false' do
+      expect(card.touch_out).to be(false)
+  end
+
+  #   it 'touch_in should change in_journey value to true' do
+  #     journey.touch_in
+  #     expect(journey.in_journey).to eq(true)
+  # end
+
+  it 'touch_out should change in_journey value to false' do
+    card.touch_out
+    expect(card.in_journey).to eq(false)
+  end
+
+  context 'When the minimum card limit is reached' do
+    minimum_balance = OysterCard::MINIMUM_BALANCE
+    it 'should raise an error: Limit reached' do
+      card.balance < minimum_balance
+      expect { card.touch_in }.to raise_error("Less than £1")
+    end
+  end
+  end
